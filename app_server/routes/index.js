fa115a36d4817ed937314c790544afde06aad648
iguessjade <iguessjade@gmail.com>
2023-08-20 17:00:37 +0530

    with-cdn

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,9 @@ const ctrlothers = require('../controller/others')
 const ctrlposts = require('../controller/articles')
 
 router.get('/',ctrlposts.home)
+router.get('/article-info',ctrlposts.articleinfo)
+
+
 router.get('/login',ctrlothers.login)
 router.get('/register',ctrlothers.register)
 router.get('/about',ctrlothers.about)
